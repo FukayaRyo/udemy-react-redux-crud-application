9d6b308c72af353074b8a75a90464074673fc5f5
FukayaRyo <fuka19960525@gmail.com>
2020-01-13 13:51:08 +0900

    apply material-ui to event_new and events_show components

--- src/compoments/events_show.js
+++ src/compoments/events_show.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux'
 import {Field, reduxForm} from 'redux-form'
 import {Link} from 'react-router-dom'
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
+
 
 import { getEvent, deleteEvent, putEvent } from '../actions'
 
@@ -21,10 +24,15 @@ class EventsShow extends Component {
     const { input, label, type, meta: { touched, error }} = field
 
     return (
-    <div>
-      <input {...input}placeholder={label} type={type} />
-    {touched && error && <span>{error}</span>}
-    </div>)
+      <TextField
+      hintText={label}
+      floatingLabelText={label}
+      type={type}
+      errorText={touched && error}
+      {...input}
+      fullWidth={true}
+    />
+    )
   }
 
   async onDeleteClick(){
@@ -40,7 +48,7 @@ class EventsShow extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting,invalid } = this.props
-    console.log(submitting)
+    const style = {margin: 12}
 
     return(
       <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -48,9 +56,9 @@ class EventsShow extends Component {
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
 
           <div>
-            <input type="submit" value="Submit" disabled={pristine || submitting || invalid }/>
-            <Link to="/">Cancel</Link>
-            <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
+          <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting|| invalid} />
+          <RaisedButton label="Cancel"  style={style} containerElement={<Link to="/" />}/>
+          <RaisedButton label="Cancel"  style={style} onClick={this.onDeleteClick}/>
           </div>
       </form>
     ) 
